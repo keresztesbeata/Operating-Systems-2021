e49826681f207c4f19764c9d214ef5a0c51f8966
keresztesbeata <keresztesbeata00@yahoo.com>
2021-03-22 20:08:22 +0200

    display variant option added

--- assignment_1/a1.c
+++ assignment_1/a1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+#define OP_VARIANT "variant"
 int main(int argc, char **argv){
     if(argc >= 2){
-        if(strcmp(argv[1], "variant") == 0){
+        if(strcmp(argv[1], OP_VARIANT) == 0){
             printf("41938\n");
         }
     }
