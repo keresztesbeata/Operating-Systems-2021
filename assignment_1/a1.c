5c1e2bcbd921cbf7066ecd5cbc701861aae6e4e5
keresztesbeata <keresztesbeata00@yahoo.com>
2021-03-22 19:48:04 +0200

    initial commit

--- assignment_1/a1.c
+++ assignment_1/a1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char **argv){
+    if(argc >= 2){
+        if(strcmp(argv[1], "variant") == 0){
+            printf("41938\n");
+        }
+    }
     return 0;
 }
