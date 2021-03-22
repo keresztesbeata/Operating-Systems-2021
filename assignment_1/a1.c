47f4f0ea4d2da30e0eb387e5d29299c8a8ca4c14
keresztesbeata <keresztesbeata00@yahoo.com>
2021-03-22 20:41:28 +0200

    added list option

--- assignment_1/a1.c
+++ assignment_1/a1.c
@@ -8,6 +8,8 @@ int main(int argc, char **argv){
     if(argc >= 2){
         if(strcmp(argv[1], OP_VARIANT) == 0){
             printf("41938\n");
+        }else if(strcmp(argv[1],OP_LIST) == 0) {
+            //todo
         }
     }
     return 0;
