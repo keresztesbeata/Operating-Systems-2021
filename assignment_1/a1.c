9492e3c5d88c6e578e4a17047894bc256f4ed244
keresztesbeata <keresztesbeata00@yahoo.com>
2021-03-24 18:17:49 +0200

    generate file header and parse its content

--- assignment_1/a1.c
+++ assignment_1/a1.c
@@ -13,27 +13,55 @@
 #define OP_LIST "list"
 #define OP_PARSE "parse"
 
+#define MAGIC_FIELD "MAGIC"
+
 #define MAX_PATH_SIZE 300
 #define MAX_NAME_SIZE 30
+#define MAX_BUF_SIZE 300
 #define MAX_NR_ELEMENTS 100
 
 #define SUCCESS 0
-#define INVALID_DIRECTORY_PATH -1
+#define INVALID_PATH -1
 #define INVALID_ARGUMENTS -2
-#define MISSING_DIR_PATH -3
+#define MISSING_PATH -3
+#define  ERR_READING_FILE -4
+
+struct section_header{
+    char sect_name[21];
+    char sect_type[6];
+    int sect_offset;
+    int sect_size;
+};
+
+
+struct header{
+    char magic[6];
+    int header_size;
+    int version;
+    int no_of_sections;
+    struct section_header * section_headers;
+};
+
 
 // command line parameters
-struct parameters{
+struct list_op_parameters{
     bool path;
     bool recursive;
     bool suffix;
     bool permission;
 };
 
-// function declarations
-int list_directory_tree(char * dir_path, char ** dir_elements, int * elem_count, char * suffix, char * permission, struct parameters detected);
+struct parse_op_parameters{
+    bool path;
+};
+
+// list the directory's content
+int list_directory_tree(char * dir_path, char ** dir_elements, int * elem_count, char * suffix, char * permission, struct list_op_parameters detected);
 void perform_op_list(int nr_parameters, char ** parameters);
 unsigned convert_permission_format(const char * permission);
+// parse files
+int parse_file_header(int fd, struct header * sf_header);
+void perform_op_parse(int nr_parameters, char ** parameters);
 
 
 int main(int argc, char **argv){
@@ -42,6 +70,8 @@ int main(int argc, char **argv){
             printf("41938\n");
         }else if(strcmp(argv[1],OP_LIST) == 0) {
             perform_op_list(argc,argv);
+        }else if(strcmp(argv[1],OP_PARSE) == 0) {
+            perform_op_parse(argc,argv);
         }
     }
     return 0;
@@ -53,10 +83,10 @@ void perform_op_list(int nr_parameters, char ** parameters) {
     int elem_count = 0;
     int return_value = SUCCESS;
 
-    struct parameters detected = {.path=false,.permission=false,.recursive=false,.suffix=false};
+    struct list_op_parameters detected = {.path=false,.permission=false,.recursive=false,.suffix=false};
 
-    char dir_path[MAX_PATH_SIZE];
-    char suffix[MAX_NAME_SIZE];
+    char dir_path[MAX_PATH_SIZE+1];
+    char suffix[MAX_NAME_SIZE+1];
     char permission[10];
 
     if(nr_parameters < 3) {
@@ -85,7 +115,7 @@ void perform_op_list(int nr_parameters, char ** parameters) {
         }
     }
     if(!detected.path) {
-        return_value = MISSING_DIR_PATH;
+        return_value = MISSING_PATH;
         goto display_error_messages;
     }
 
@@ -107,11 +137,11 @@ void perform_op_list(int nr_parameters, char ** parameters) {
     display_error_messages:
     if(return_value != SUCCESS) {
         printf("ERROR\n");
-        if (return_value == INVALID_DIRECTORY_PATH)
+        if (return_value == INVALID_PATH)
             printf("Invalid directory path\n");
         else if (return_value == INVALID_ARGUMENTS)
             printf(" USAGE: list [recursive] <filtering_options> path=<dir_path> \nThe order of the options is not relevant.\n");
-        else if (return_value == MISSING_DIR_PATH)
+        else if (return_value == MISSING_PATH)
             printf("No directory path was specified.\n");
     }
 
@@ -119,8 +149,8 @@ void perform_op_list(int nr_parameters, char ** parameters) {
 
 unsigned convert_permission_format(const char * permission) {
     //generate permission bits
-    unsigned p_rights = 0;
-    unsigned bit = 1;
+    unsigned p_rights = 0u;
+    unsigned bit = 1u;
     for(int i=8;i>=0;i--) {
         if(permission[i] != '-') {
             p_rights = p_rights | bit;
@@ -130,16 +160,16 @@ unsigned convert_permission_format(const char * permission) {
     return p_rights;
 }
 
-int list_directory_tree(char * dir_path, char ** dir_elements, int * elem_count, char * suffix, char * permission,struct parameters detected){
+int list_directory_tree(char * dir_path, char ** dir_elements, int * elem_count, char * suffix, char * permission,struct list_op_parameters detected){
     DIR* dir;
     struct dirent *entry;
     struct stat inode;
-    char abs_entry_path[MAX_PATH_SIZE];
+    char abs_entry_path[MAX_PATH_SIZE+1];
     int return_value = SUCCESS;
     // open the directory
     dir = opendir(dir_path);
     if(dir == 0) {
-        return_value = INVALID_DIRECTORY_PATH;
+        return_value = INVALID_PATH;
         goto clean_up;
     }
     // iterate through the directory's content
@@ -187,4 +217,138 @@ int list_directory_tree(char * dir_path, char ** dir_elements, int * elem_count,
         closedir(dir);
     }
     return return_value;
+}
+
+int read_line(int fd, char * line, int * length) {
+    int index = 0;
+    char ch;
+    while((index < MAX_BUF_SIZE) && (read(fd,&ch,1) > 0) && (ch != '\n')) {
+        line[index] = ch;
+        index++;
+    }
+    line[index]='\0';
+    *length = index;
+    return 0;
+}
+
+int parse_file_header(int fd, struct header * sf_header) {
+    int pos = lseek(fd,0,SEEK_SET);
+    if(pos != 0) {
+        return -1;
+    }
+    read(fd,sf_header->magic,4);
+    sf_header->magic[4]='\0';
+    read(fd,&sf_header->header_size,2);
+    read(fd,&sf_header->version,2);
+    read(fd,&sf_header->no_of_sections,1);
+    sf_header->section_headers = (struct section_header*)malloc(sizeof(struct section_header)*sf_header->no_of_sections);
+    for(int i=0;i<sf_header->no_of_sections;i++) {
+        read(fd,&sf_header->section_headers[i].sect_name,19);
+        sf_header->section_headers[i].sect_name[20] = '\0';
+        read(fd,&sf_header->section_headers[i].sect_type,4);
+        sf_header->section_headers[i].sect_type[4] = '\0';
+        read(fd,&sf_header->section_headers[i].sect_offset,4);
+        read(fd,&sf_header->section_headers[i].sect_size,4);
+        char ch[3];
+        read(fd,&ch,2);
+        ch[2]='\0';
+        if(ch[0] == (char)0x0 && ch[1] == (char)0xA) {
+            printf("found line ending\n");
+        }
+    }
+    return SUCCESS;
+}
+
+
+void perform_op_parse(int nr_parameters, char ** parameters){
+    int return_value = SUCCESS;
+    struct header * sf_header = (struct header*)malloc(sizeof(struct header));
+    int fd;
+    char file_path[MAX_PATH_SIZE+1];
+    struct parse_op_parameters detected ={.path = false};
+
+    if(nr_parameters < 3) {
+        return_value = INVALID_ARGUMENTS;
+        goto display_error_messages;
+    }
+    for(int i=2;i<nr_parameters;i++) {
+            char * filter_option = strtok(parameters[i],"=");
+            char * filter_value = parameters[i] + strlen(filter_option) + 1;
+            if(strcmp(filter_option,"path") == 0) {
+                // detected path argument
+                strcpy(file_path,filter_value);
+                detected.path = true;
+            }
+        }
+    if(!detected.path) {
+        return_value = MISSING_PATH;
+        goto display_error_messages;
+    }
+
+    /*
+     * first write to the file
+     */
+
+    fd = open(file_path,O_WRONLY);
+    if(fd < 0) {
+        return_value = INVALID_PATH;
+        goto display_error_messages;
+    }
+
+    write(fd,"1A4P",4);
+    int header_size = 200;
+    write(fd,&header_size,2);
+    int version = 47;
+    write(fd,&version,2);
+    int nr_sections = 3;
+    write(fd,&nr_sections,1);
+    for(int i=0;i<nr_sections;i++) {
+        write(fd, "nothing else matters",19);
+        write(fd,"type",4);
+        int offset = 0x0100;
+        write(fd,&offset,4);
+        int size = 20;
+        write(fd,&size,4);
+        int line_ending = 0x0A;
+        write(fd,&line_ending,2);
+    }
+
+    if(fd > 0) {
+        close(fd);
+    }
+
+    fd = open(file_path,O_RDONLY);
+    if(fd < 0) {
+        return_value = INVALID_PATH;
+        goto display_error_messages;
+    }
+    // parse file's header
+    return_value = parse_file_header(fd,sf_header);
+
+    printf("magic = %s\n",sf_header->magic);
+    printf("header_size = %d\n",sf_header->header_size);
+    printf("version = %d\n",sf_header->version);
+    printf("nr_sections = %d\n",sf_header->no_of_sections);
+    for(int i=0;i<sf_header->no_of_sections;i++) {
+        printf("section[%d] :\n-name = %s\n-type = %s\n-offset = %x\n-size = %d\n",i,
+               sf_header->section_headers[i].sect_name,
+               sf_header->section_headers[i].sect_type,
+               sf_header->section_headers[i].sect_offset,
+               sf_header->section_headers[i].sect_size);
+
+    }
+
+
+    display_error_messages:
+    if(return_value != SUCCESS) {
+        printf("ERROR\n");
+        if (return_value == INVALID_PATH)
+            printf("Invalid file path\n");
+        else if (return_value == INVALID_ARGUMENTS)
+            printf(" USAGE: parse  path=<file_path> \nThe order of the options is not relevant.\n");
+        else if (return_value == MISSING_PATH)
+            printf("No file path was specified.\n");
+        else if (return_value == ERR_READING_FILE)
+            printf("Error reading from file.\n");
+    }
 }
