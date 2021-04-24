c8c52c1de4c5cc345414539e8187f42f2738edf0
keresztesbeata <keresztesbeata00@yahoo.com>
2021-04-24 09:19:43 +0300

    Assignemnt for threads synchronization

---
+++ assignment_2/cmake-build-debug/CMakeFiles/assignment_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment_2.dir/main.c.o"
+  "assignment_2"
+  "assignment_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
