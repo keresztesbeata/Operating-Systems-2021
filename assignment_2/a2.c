309370c09e2a7444713d0ea93feb420262d803a9
keresztesbeata <keresztesbeata00@yahoo.com>
2021-04-24 16:36:36 +0300

    tested return value of functions and added error messages

--- assignment_2/a2.c
+++ assignment_2/a2.c
@@ -9,12 +9,17 @@
 #include <semaphore.h>
 #define ERROR_CREATING_PROCESS 1
 #define ERROR_CREATING_THREAD 2
-#define ERROR_CREATING_SEMAPHORE 3
+#define ERROR_JOINING_THREAD 3
+#define ERROR_LOCKS 4
+#define ERROR_COND_VAR 5
+// #define ERROR_CREATING_SEMAPHORE 3
 
-#define PRINT_ERROR_CREATING_PROCESS {perror("Cannot create new process"); exit(ERROR_CREATING_PROCESS);}
-#define PRINT_ERROR_CREATING_THREAD {perror("Error creating a new thread"); exit(ERROR_CREATING_THREAD);}
-#define PRINT_ERROR_CREATING_SEMAPHORE {perror("Error creating the semaphore");exit(ERROR_CREATING_SEMAPHORE);}
-
+#define PRINT_ERROR_CREATING_PROCESS { perror("Cannot create new process."); }
+#define PRINT_ERROR_CREATING_THREAD { perror("Error creating a new thread."); }
+#define PRINT_ERROR_JOINING_THREAD { perror("Error joining a thread."); }
+#define PRINT_ERROR_LOCKS { perror("Error trying to unlock or acquire a lock."); }
+#define PRINT_ERROR_COND_VAR { perror("Error waiting for or signaling a condition."); }
+// #define PRINT_ERROR_CREATING_SEMAPHORE {perror("Error creating the semaphore");exit(ERROR_CREATING_SEMAPHORE);}
 
 typedef struct thread_args{
     int th_id;
@@ -46,55 +51,60 @@ void create_process(int parent_id) {
 }
 
 void * do_work(void * arg) {
-    int status;
+    int error_code = 0;
+    int * return_value = &error_code;
     thread_args_t th_arg = *(thread_args_t*)arg;
-    status = pthread_mutex_lock(&lock);
-    if (status != 0)
-        return 0;
+    if(pthread_mutex_lock(&lock) != 0) {
+        error_code = ERROR_LOCKS;
+        goto finish;
+    }
     while (th_arg.th_id == 2 && !allowed_to_start){
-        status = pthread_cond_wait(&cond_start, &lock);
-        if (status != 0)
-            return 0;
+        if(pthread_cond_wait(&cond_start, &lock) != 0) {
+            error_code = ERROR_COND_VAR;
+            goto finish;
+        }
     }
     info(BEGIN, th_arg.pr_id, th_arg.th_id);
     if(th_arg.th_id == 3) {
         allowed_to_start = true;
-        status = pthread_cond_broadcast(&cond_start);
-        if (status != 0)
-            return 0;
+        if(pthread_cond_broadcast(&cond_start) != 0) {
+            error_code = ERROR_COND_VAR;
+         goto finish;
+        }
     }
-    status = pthread_mutex_unlock(&lock);
-    if (status != 0)
-        return 0;
-    /* ----------------------------------------*/
-    status = pthread_mutex_lock(&lock);
-    if (status != 0)
-        return 0;
+    if(pthread_mutex_unlock(&lock) != 0) {
+        error_code = ERROR_LOCKS;
+        goto finish;
+    }
+    if(pthread_mutex_lock(&lock) != 0) {
+        error_code = ERROR_LOCKS;
+        goto finish;
+    }
     while (th_arg.th_id == 3 && !allowed_to_finish){
-        status = pthread_cond_wait(&cond_end, &lock);
-        if (status != 0)
-            return 0;
+        if(pthread_cond_wait(&cond_end, &lock) != 0) {
+            error_code = ERROR_COND_VAR;
+            goto finish;
+        }
     }
     info(END, th_arg.pr_id, th_arg.th_id);
     if(th_arg.th_id == 2) {
         allowed_to_finish = true;
-        status = pthread_cond_broadcast(&cond_end);
-        if (status != 0)
-            return 0;
+        if(pthread_cond_broadcast(&cond_end) != 0) {
+            error_code = ERROR_COND_VAR;
+            goto finish;
+        }
     }
-    status = pthread_mutex_unlock(&lock);
-    if (status != 0)
-        return 0;
-    return 0;
+    if(pthread_mutex_unlock(&lock) != 0) {
+        error_code = ERROR_LOCKS;
+        goto finish;
+    }
+
+    finish:
+    return return_value;
 }
 
-void create_thread(int nr) {
-    if (pthread_create(&th[nr], NULL, do_work,&th_args[nr]) != 0) {
-        PRINT_ERROR_CREATING_THREAD
-    }
-}
-
-void synchronizing_threads_in_same_process(){
+int synchronizing_threads_in_same_process(){
+    int error_code = 0;
     // initialize the thread arguments
     for(int i=0;i<4;i++) {
         th_args[i].pr_id = 3;
@@ -102,12 +112,27 @@ void synchronizing_threads_in_same_process(){
     }
     // create the threads
     for(int i=0;i<4;i++) {
-        create_thread(i);
+        if (pthread_create(&th[i], NULL, do_work,&th_args[i]) != 0) {
+            error_code = ERROR_CREATING_THREAD;
+        }
     }
     // join the created threads
     for(int i=0;i<4;i++) {
-        pthread_join(th[i], NULL);
+        int status = 0;
+        if(pthread_join(th[i], (void *)&status) != 0) {
+            error_code = ERROR_JOINING_THREAD;
+        }
+        else if(status == ERROR_LOCKS) {
+            PRINT_ERROR_LOCKS
+        }else if(status == ERROR_COND_VAR) {
+            PRINT_ERROR_COND_VAR
+        }
     }
+    return error_code;
+}
+
+void threads_barrier() {
+
 }
 
 int main(){
@@ -124,7 +149,12 @@ int main(){
     create_process(6);
 
     if(p_id == 3) {
-        synchronizing_threads_in_same_process();
+        int return_code = synchronizing_threads_in_same_process();
+        if(return_code == ERROR_CREATING_THREAD) {
+            PRINT_ERROR_CREATING_THREAD
+        }else if(return_code == ERROR_JOINING_THREAD) {
+            PRINT_ERROR_JOINING_THREAD
+        }
     }
     // wait for the child processes to terminate
     while(wait(&return_status) > 0) {
