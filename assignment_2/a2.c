491d589684dfe1c44f834aa9686f0c643683c155
keresztesbeata <keresztesbeata00@yahoo.com>
2021-04-26 16:13:36 +0300

    solved thread barriers, but it exceeds the time limit only at test2

--- assignment_2/a2.c
+++ assignment_2/a2.c
@@ -25,7 +25,7 @@
 
 #define SEM_ENTER "/sem_enter"
 #define SEM_LEAVE "/sem_leave"
-#define SEM_STAY "/sem_stay"
+#define SEM_STAY "/sem_barrier"
 #define SEM_COUNT "/sem_count"
 
 typedef struct thread_args{
@@ -37,7 +37,7 @@ int p_id = 1; // parent's id
 int c_nr = 1; // child count
 
 sem_t sem_end_after_th2,sem_start_after_th3;
-sem_t *sem_enter,*sem_leave,*sem_stay,*sem_count;
+sem_t sem_enter,sem_leave,sem_barrier,sem_count;
 sem_t *sem_start_th4, *sem_start_th3;
 
 void create_process(int parent_id) {
@@ -138,35 +138,41 @@ bool allow_to_leave = true;
 
 void * task_of_threads_in_p7(void * arg) {
     thread_args_t th_arg = *(thread_args_t*)arg;
-    int value;
-    sem_getvalue(sem_count,&value);
-    if(!allow_to_leave && value >= 3) {
-        //printf("you can leave now\n");
-        V(sem_leave);
-    }
-    P(sem_enter);
+    P(&sem_enter);
     // at most 4 threads can enter at a time
     info(BEGIN, th_arg.pr_id, th_arg.th_id);
+    // block thread 15 until the room is full
     if(th_arg.th_id == 15) {
         allow_to_leave = false;
-        P(sem_leave);
+        P(&sem_leave);
     }
-
+   // printf("allowed to leave = %d\n",allow_to_leave);
+    // the other threads after 15 should be blocked too
     if(th_arg.th_id != 15 && !allow_to_leave) {
-        //printf("thread %d is blocked\n",th_arg.th_id);
-        V(sem_count);
-        P(sem_leave);
-       // printf("t %d signal for the next to leave\n",th_arg.th_id);
-        V(sem_leave);
+       //   printf("thread %d is blocked\n",th_arg.th_id);
+        V(&sem_count); // event counter, counts how many threads have entered after 15
+        // the last one will unblock thread 15 first
+        int value;
+        sem_getvalue(&sem_count,&value);
+      //  printf("allowed to leave = %d\n",allow_to_leave);
+      //  printf("count = %d\n",value);
+        if(value >= 3) {
+            // signal thread 15 that it is now safe to leave the room
+        //     printf("you can leave now\n");
+            V(&sem_leave);
+        }
+        P(&sem_barrier);
+       //  printf("t %d signal for the next to leave\n",th_arg.th_id);
+        V(&sem_barrier); // unblock the next waiting thread
     }
     info(END, th_arg.pr_id, th_arg.th_id);
 
     if(th_arg.th_id == 15) {
         allow_to_leave = true;
-        V(sem_leave);
+        V(&sem_barrier);
     }
 
-    V(sem_enter);
+    V(&sem_enter);
     return 0;
 }
 
@@ -174,43 +180,50 @@ int threads_barrier() {
     int error_code = 0;
     pthread_t th[38];
     thread_args_t th_args[38];
-    // create and initialize the semaphores
-    sem_enter = sem_open(SEM_ENTER, O_CREAT, 0600, 4);
-    if (sem_enter == SEM_FAILED) {
+    if (sem_init(&sem_enter, 1, 4) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
     int value;
-    sem_getvalue(sem_enter,&value);
-    printf("value sem_enter = %d\n",value);
+    sem_getvalue(&sem_enter,&value);
+    printf("value = %d\n",value);
 
-    sem_leave = sem_open(SEM_LEAVE, O_CREAT, 0600, 0);
-    if (sem_leave == SEM_FAILED) {
+    if (sem_init(&sem_leave, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
-    sem_getvalue(sem_leave,&value);
-    printf("value sem_leave= %d\n",value);
+    sem_getvalue(&sem_leave,&value);
+    printf("value = %d\n",value);
 
-    sem_count= sem_open(SEM_COUNT, O_CREAT, 0600, 0);
-    if (sem_count == SEM_FAILED) {
+    if (sem_init(&sem_barrier, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
-    sem_getvalue(sem_count,&value);
-    printf("value sem_count= %d\n",value);
+    sem_getvalue(&sem_barrier,&value);
+    printf("value = %d\n",value);
 
-   //  initialize the thread arguments
+    if (sem_init(&sem_count, 1, 0) < 0) {
+        error_code = ERROR_CREATING_SEMAPHORE;
+        goto finish;
+    }
+    sem_getvalue(&sem_count,&value);
+    printf("value = %d\n",value);
+
+
+    // initialize the thread arguments
     for(int i=0;i<38;i++) {
         th_args[i].pr_id = 7;
         th_args[i].th_id = i+1;
     }
-
+    if (pthread_create(&th[14], NULL, task_of_threads_in_p7, &th_args[14]) != 0) {
+        error_code = ERROR_CREATING_THREAD;
+    }
     // create the threads
     for(int i=0;i<38;i++) {
-        if (pthread_create(&th[i], NULL, task_of_threads_in_p7, &th_args[i]) != 0) {
-            error_code = ERROR_CREATING_THREAD;
-            goto finish;
+        if(i!=14) {
+            if (pthread_create(&th[i], NULL, task_of_threads_in_p7, &th_args[i]) != 0) {
+                error_code = ERROR_CREATING_THREAD;
+            }
         }
     }
     // join the created threads
@@ -218,12 +231,14 @@ int threads_barrier() {
         int status = 0;
         if(pthread_join(th[i], (void *)&status) != 0) {
             error_code = ERROR_JOINING_THREAD;
+            goto finish;
         }
     }
     finish:
-    sem_unlink(SEM_ENTER);
-    sem_unlink(SEM_LEAVE);
-    sem_unlink(SEM_COUNT);
+    sem_destroy(&sem_enter);
+    sem_destroy(&sem_leave);
+    sem_destroy(&sem_barrier);
+    sem_destroy(&sem_count);
     return error_code;
 }
 
@@ -276,7 +291,9 @@ int synchronizing_threads_in_diff_processes() {
     }
     finish:
     sem_close(sem_start_th3);
+    sem_unlink(SEM_START_TH3);
     sem_close(sem_start_th4);
+    sem_unlink(SEM_START_TH4);
     return error_code;
 }
 
@@ -293,26 +310,26 @@ int main(){
     create_process(4);
     create_process(6);
 
-//    if(p_id == 2) {
-//        int return_code = synchronizing_threads_in_diff_processes();
-//        if(return_code == ERROR_CREATING_THREAD) {
-//            PRINT_ERROR_CREATING_THREAD
-//        }else if(return_code == ERROR_JOINING_THREAD) {
-//            PRINT_ERROR_JOINING_THREAD
-//        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
-//            PRINT_ERROR_CREATING_SEMAPHORE
-//        }
-//    }
-//    if(p_id == 3) {
-//        int return_code = synchronizing_threads_in_same_process();
-//        if(return_code == ERROR_CREATING_THREAD) {
-//            PRINT_ERROR_CREATING_THREAD
-//        }else if(return_code == ERROR_JOINING_THREAD) {
-//            PRINT_ERROR_JOINING_THREAD
-//        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
-//            PRINT_ERROR_CREATING_SEMAPHORE
-//        }
-//    }
+    if(p_id == 2) {
+        int return_code = synchronizing_threads_in_diff_processes();
+        if(return_code == ERROR_CREATING_THREAD) {
+            PRINT_ERROR_CREATING_THREAD
+        }else if(return_code == ERROR_JOINING_THREAD) {
+            PRINT_ERROR_JOINING_THREAD
+        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
+            PRINT_ERROR_CREATING_SEMAPHORE
+        }
+    }
+    if(p_id == 3) {
+        int return_code = synchronizing_threads_in_same_process();
+        if(return_code == ERROR_CREATING_THREAD) {
+            PRINT_ERROR_CREATING_THREAD
+        }else if(return_code == ERROR_JOINING_THREAD) {
+            PRINT_ERROR_JOINING_THREAD
+        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
+            PRINT_ERROR_CREATING_SEMAPHORE
+        }
+    }
     if(p_id == 7) {
         int return_code = threads_barrier();
         if(return_code == ERROR_CREATING_THREAD) {
