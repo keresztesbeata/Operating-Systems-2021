b8024fc7ac2fd0f265280bc34816fc780381eb89
keresztesbeata <keresztesbeata00@yahoo.com>
2021-04-26 20:49:13 +0300

    all tests complete successfully, cleaned up code

--- assignment_2/a2.c
+++ assignment_2/a2.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <pthread.h>
-#include <stdbool.h>
 #include "a2_helper.h"
 #include <semaphore.h>
 #include <fcntl.h>
-#include <errno.h>
 
 #define ERROR_CREATING_PROCESS 1
 #define ERROR_CREATING_THREAD 2
@@ -28,19 +25,32 @@ typedef struct thread_args{
     int pr_id;
 }thread_args_t;
 
-int p_id = 1; // parent's id
-int c_nr = 1; // child count
+int p_id = 1; // parent process's id
+int c_nr = 1; // child process count
 
-sem_t sem_end_after_th2,sem_start_after_th3;
-sem_t sem_enter,sem_leave,sem_barrier,sem_allow;
-sem_t *sem_start_th4, *sem_start_th3;
+// semaphores used by process 2 and 3
+sem_t sem_end_after_th2;
+sem_t sem_start_after_th3;
+sem_t *sem_start_th4;
+sem_t *sem_start_th3;
 
+// semaphores used by process 7
+sem_t sem_limit;
+sem_t sem_leave;
+sem_t sem_barrier;
+sem_t sem_enter;
+
+/*
+ * Lets the process with the given process id to create a new child process.
+ */
 void create_process(int parent_id) {
+    // increment the child count whenever a new child process is created
     c_nr++;
     if(p_id == parent_id) {
         pid_t pid = fork();
         if(pid < 0) PRINT_ERROR_CREATING_PROCESS
-        if(pid == 0) { // inside child process
+        if(pid == 0) {
+            // inside child process
             p_id = c_nr;
             info(BEGIN, p_id, 0);
         }
@@ -59,24 +69,28 @@ void V(sem_t *sem)
 
 void * task_of_threads_in_p3(void * arg) {
     thread_args_t th_arg = *(thread_args_t*)arg;
-    if(th_arg.th_id == 2) { // thread 2 has to start after thread 3
+    // thread 2 has to start after thread 3
+    if(th_arg.th_id == 2) {
         P(&sem_start_after_th3);
     }
-    if(th_arg.th_id == 4) { // thread 4 has to start after thread 2 from P2
+    // thread 4 has to start after thread 2 from P2
+    if(th_arg.th_id == 4) {
         P(sem_start_th4);
     }
     info(BEGIN, th_arg.pr_id, th_arg.th_id);
     if(th_arg.th_id == 3) {
         V(&sem_start_after_th3);
     }
-    if(th_arg.th_id == 3) { // thread 3 has to leave after thread 2
+    // thread 3 has to leave after thread 2
+    if(th_arg.th_id == 3) {
         P(&sem_end_after_th2);
     }
     info(END, th_arg.pr_id, th_arg.th_id);
     if(th_arg.th_id == 2) {
         V(&sem_end_after_th2);
     }
-    if(th_arg.th_id == 4) { // when thread 4 finishes it must signal thread 3 from P2 to start
+    // when thread 4 finishes it must signal thread 3 from P2 to start
+    if(th_arg.th_id == 4) {
         V(sem_start_th3);
     }
     return 0;
@@ -86,19 +100,23 @@ int synchronizing_threads_in_same_process(){
     int error_code = 0;
     pthread_t th[4];
     thread_args_t th_args[4];
+    // create unnamed semaphore for controlling when should thread 2 from process 3 start (only after thread 3)
     if (sem_init(&sem_start_after_th3, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
+    // create unnamed semaphore for controlling when should thread 3 from process 3 end (after thread 2)
     if (sem_init(&sem_end_after_th2, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
+    // open named semaphore for controlling when should thread 3 from process 2 start
     sem_start_th3 = sem_open(SEM_START_TH3,O_CREAT,0600,0);
     if(sem_start_th3 == SEM_FAILED) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
+    // open named semaphore for controlling when should thread 4 from process 3 start
     sem_start_th4 = sem_open(SEM_START_TH4,O_CREAT,0600,0);
     if(sem_start_th4== SEM_FAILED) {
         error_code = ERROR_CREATING_SEMAPHORE;
@@ -124,36 +142,34 @@ int synchronizing_threads_in_same_process(){
         }
     }
     finish:
+    // destroy the semaphores
     sem_destroy(&sem_start_after_th3);
     sem_destroy(&sem_end_after_th2);
     return error_code;
 }
 
-bool allowed_to_enter = false;
-
 void * task_of_threads_in_p7(void * arg) {
     thread_args_t th_arg = *(thread_args_t*)arg;
     // threads who attempt to enter before thread 15 are blocked
-    if(th_arg.th_id != 15 && !allowed_to_enter) {
-        P(&sem_allow);
+    if(th_arg.th_id != 15) {
+        P(&sem_enter);
     }
     // unblock the next waiting thread
-    V(&sem_allow);
+    V(&sem_enter);
     // at most 4 threads can enter at a time
-    P(&sem_enter);
+    P(&sem_limit);
     info(BEGIN, th_arg.pr_id, th_arg.th_id);
 
     if(th_arg.th_id == 15) {
-        allowed_to_enter = true;
         // allow the next 3 threads to enter the room
-        V(&sem_allow);
+        V(&sem_enter);
         // wait until the room is full
         P(&sem_leave);
     }
 
     if(th_arg.th_id != 15) {
         int value;
-        sem_getvalue(&sem_enter,&value);
+        sem_getvalue(&sem_limit, &value);
         // check if all permissions were taken <=> there are already 4 threads inside together with thread 15
         if(value <= 0) {
             // signal thread 15 to leave the room
@@ -169,39 +185,40 @@ void * task_of_threads_in_p7(void * arg) {
         // lift the barrier after leaving the room
         V(&sem_barrier);
     }
-    V(&sem_enter);
+    V(&sem_limit);
     return 0;
 }
+
 int threads_barrier() {
     int error_code = 0;
     pthread_t th[38];
     thread_args_t th_args[38];
-    if (sem_init(&sem_enter, 1, 4) < 0) {
+    // create semaphore for limiting the number of threads in a room
+    if (sem_init(&sem_limit, 1, 4) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
-
+    // create semaphore for blocking thread 15, not allowing it to leave until there have gathered other 3 threads inside the room
     if (sem_init(&sem_leave, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
-
+    // create semaphore for blocking the threads entering after thread 15, so that they wouldn't leave before 15
     if (sem_init(&sem_barrier, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
-
-    if (sem_init(&sem_allow, 1, 0) < 0) {
+    // create a semaphore for blocking the threads that try to enter before thread 15, so that when th 15 enters there
+    // would always be at least another 3 threads to unblock it
+    if (sem_init(&sem_enter, 1, 0) < 0) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
-
     // initialize the thread arguments
     for(int i=0;i<38;i++) {
         th_args[i].pr_id = 7;
         th_args[i].th_id = i+1;
     }
-
     // create the threads
     for(int i=0;i<38;i++) {
         if (pthread_create(&th[i], NULL, task_of_threads_in_p7, &th_args[i]) != 0) {
@@ -217,22 +234,25 @@ int threads_barrier() {
         }
     }
     finish:
-    sem_destroy(&sem_enter);
+    // destroy the semaphores
+    sem_destroy(&sem_limit);
     sem_destroy(&sem_leave);
     sem_destroy(&sem_barrier);
-    sem_destroy(&sem_allow);
+    sem_destroy(&sem_enter);
     return error_code;
 }
 
 void * task_of_threads_in_p2(void * arg) {
     thread_args_t th_arg = *(thread_args_t*)arg;
-    if(th_arg.th_id == 3) { // thread 3 must start after thread 4 from P3 finishes
+    // thread 3 must start after thread 4 from P3 finishes
+    if(th_arg.th_id == 3) {
         P(sem_start_th3);
     }
     info(BEGIN, th_arg.pr_id, th_arg.th_id);
 
     info(END, th_arg.pr_id, th_arg.th_id);
-    if(th_arg.th_id == 2) { // thread 2 has to signal thread 4 from P3 to start
+    // thread 2 has to signal thread 4 from P3 to start
+    if(th_arg.th_id == 2) {
         V(sem_start_th4);
     }
     return 0;
@@ -242,11 +262,13 @@ int synchronizing_threads_in_diff_processes() {
     int error_code = 0;
     pthread_t th[5];
     thread_args_t th_args[5];
+    // create a named semaphore which controls when should thread 3 from process 2 begin
     sem_start_th3 = sem_open(SEM_START_TH3,O_CREAT,0600,0);
     if(sem_start_th3 == SEM_FAILED) {
         error_code = ERROR_CREATING_SEMAPHORE;
         goto finish;
     }
+    // create a named semaphore which controls when should thread 4 from process 3 begin
     sem_start_th4 = sem_open(SEM_START_TH4,O_CREAT,0600,0);
     if(sem_start_th4== SEM_FAILED) {
         error_code = ERROR_CREATING_SEMAPHORE;
@@ -272,6 +294,7 @@ int synchronizing_threads_in_diff_processes() {
         }
     }
     finish:
+    // close and unlink the named semaphores
     sem_close(sem_start_th3);
     sem_unlink(SEM_START_TH3);
     sem_close(sem_start_th4);
@@ -292,26 +315,26 @@ int main(){
     create_process(4);
     create_process(6);
 
-//    if(p_id == 2) {
-//        int return_code = synchronizing_threads_in_diff_processes();
-//        if(return_code == ERROR_CREATING_THREAD) {
-//            PRINT_ERROR_CREATING_THREAD
-//        }else if(return_code == ERROR_JOINING_THREAD) {
-//            PRINT_ERROR_JOINING_THREAD
-//        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
-//            PRINT_ERROR_CREATING_SEMAPHORE
-//        }
-//    }
-//    if(p_id == 3) {
-//        int return_code = synchronizing_threads_in_same_process();
-//        if(return_code == ERROR_CREATING_THREAD) {
-//            PRINT_ERROR_CREATING_THREAD
-//        }else if(return_code == ERROR_JOINING_THREAD) {
-//            PRINT_ERROR_JOINING_THREAD
-//        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
-//            PRINT_ERROR_CREATING_SEMAPHORE
-//        }
-//    }
+    if(p_id == 2) {
+        int return_code = synchronizing_threads_in_diff_processes();
+        if(return_code == ERROR_CREATING_THREAD) {
+            PRINT_ERROR_CREATING_THREAD
+        }else if(return_code == ERROR_JOINING_THREAD) {
+            PRINT_ERROR_JOINING_THREAD
+        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
+            PRINT_ERROR_CREATING_SEMAPHORE
+        }
+    }
+    if(p_id == 3) {
+        int return_code = synchronizing_threads_in_same_process();
+        if(return_code == ERROR_CREATING_THREAD) {
+            PRINT_ERROR_CREATING_THREAD
+        }else if(return_code == ERROR_JOINING_THREAD) {
+            PRINT_ERROR_JOINING_THREAD
+        }else if(return_code == ERROR_CREATING_SEMAPHORE) {
+            PRINT_ERROR_CREATING_SEMAPHORE
+        }
+    }
     if(p_id == 7) {
         int return_code = threads_barrier();
         if(return_code == ERROR_CREATING_THREAD) {
