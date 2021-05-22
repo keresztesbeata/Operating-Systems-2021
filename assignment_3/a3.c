98f5572946b29c447d303c09eca63ef71d7d9fb8
keresztesbeata <keresztesbeata00@yahoo.com>
2021-05-22 15:11:27 +0300

    writing to shared memory

--- assignment_3/a3.c
+++ assignment_3/a3.c
@@ -13,7 +13,8 @@ ERR_OPENING_PIPE,
 ERR_WRITING_TO_PIPE,
 ERR_READING_FROM_PIPE,
 ERR_CREATING_SHARED_MEMORY,
-ERR_CREATING_MAPPING
+ERR_CREATING_MAPPING,
+ERR_WRITING_TO_SHARED_MEMORY
 }return_status_t;
 
 #define MSG_ERROR "ERROR"
@@ -21,11 +22,19 @@ ERR_CREATING_MAPPING
 
 #define MSG_CONNECT "CONNECT"
 #define MSG_PING "PING"
-#define MSG_SH_MEM "CREATE_SHM"
+#define MSG_CREATE_SH_MEM "CREATE_SHM"
+#define MSG_WRITE_TO_SH_MEM "WRITE_TO_SHM"
+#define MSG_MAP_FILE "MAP_FILE"
+#define MSG_READ_FROM_FILE_OFFSET "READ_FROM_FILE_OFFSET"
+#define MSG_READ_FROM_FILE_SECTION "READ_FROM_FILE_SECTION"
+#define MSG_READ_FROM_LOGICAL_SPACE_OFFSET "READ_FROM_LOGICAL_SPACE_OFFSET"
+#define MSG_EXIT "EXIT"
 
 #define MAX_REQUEST_LENGTH 100
 
 #define MAGIC_NR "1A4P"
+#define SH_MEM_NAME "/o9gGHlSV"
+#define SH_MEM_SIZE 4989424
 
 #pragma pack(push,1)
 typedef struct s_sect_header{
@@ -53,6 +62,9 @@ typedef struct sf_format{
 
 #define RESP_PIPE_NAME "RESP_PIPE_41938"
 #define REQ_PIPE_NAME "REQ_PIPE_41938"
+
+
+
 /*
  * REQUEST:
  * <req_name> <params> ...
@@ -74,23 +86,25 @@ int open_named_pipe(int * fd, char * name, int flag);
 int write_string_field(int fd, char * param);
 int write_number_field(int fd, unsigned int param);
 int read_string_field(int fd, char * param);
-int read_number_field(int fd, int * param);
+int read_number_field(int fd, unsigned int * param);
 
-int read_and_handle_request(int fd_read, int fd_write);
-int handle_ping_request(int fd_write);
-int handle_create_shared_memory_request(int fd_read, int fd_write);
-
+int read_and_handle_request();
+int handle_ping_request();
+int handle_create_shared_memory_request();
+int handle_write_to_shared_memory_request();
 
 int create_shared_memory(int * fd, char * name, int size);
 int map_shared_memory(int fd, int size, char * mapped_data);
 
 bool is_valid_sf_format(sf_header_t sf_header);
 
+int fd_shm = -1;
+char * shared_mem_data = NULL;
+int fd_read = -1, fd_write = -1;
+bool exit_loop;
 
 int main() {
     int status = SUCCESS;
-    int fd_read = -1, fd_write = -1;
-
     status = create_named_pipe(RESP_PIPE_NAME);
     if(status != SUCCESS) goto clean_up;
 
@@ -102,17 +116,18 @@ int main() {
 
     printf("%s\n",MSG_SUCCESS);
 
-    /* affirm connected to pipe */
+    /* affirm connection to the pipe */
     status = write_string_field(fd_write, MSG_CONNECT);
     if(status != SUCCESS) goto clean_up;
-
-    read_and_handle_request(fd_read, fd_write);
 
     /*
      * read request from REQ_PIPE
      * handle request
      * write back to RESP_PIPE the result
      */
+    while(!exit_loop)
+        read_and_handle_request(fd_read, fd_write);
+
 
     clean_up:
     if(status != SUCCESS)
@@ -123,21 +138,14 @@ int main() {
         close(fd_write);
     unlink(REQ_PIPE_NAME);
     unlink(RESP_PIPE_NAME);
-
+    if(fd_shm > 0) {
+        munmap(shared_mem_data,SH_MEM_SIZE);
+        close(fd_shm);
+        shm_unlink(SH_MEM_NAME);
+    }
     return status;
 }
-char * select_error_message(return_status_t status){
-    switch (status) {
-        case ERR_CREATING_PIPE: return "cannot create pipe";
-        case ERR_OPENING_PIPE: return "cannot open pipe";
-        case ERR_READING_FROM_PIPE: return "cannot read from request pipe";
-        case ERR_WRITING_TO_PIPE: return "cannot write to response pipe";
-        case ERR_CREATING_SHARED_MEMORY: return "cannot create shared memory";
-        case ERR_CREATING_MAPPING: return "cannot map shared memory";
-        default: return "";
-    }
-}
-int read_and_handle_request(int fd_read, int fd_write){
+int read_and_handle_request(){
     int status = SUCCESS;
     /* read request */
     char request_name[MAX_REQUEST_LENGTH + 1];
@@ -146,13 +154,24 @@ int read_and_handle_request(int fd_read, int fd_write){
     /* decode adn handle request */
     if(strncmp(request_name, MSG_PING, strlen(MSG_PING)) == 0) {
         status = handle_ping_request(fd_write);
-    }else if(strncmp(request_name, MSG_SH_MEM, strlen(MSG_SH_MEM)) == 0) {
+    }else if(strncmp(request_name, MSG_CREATE_SH_MEM, strlen(MSG_CREATE_SH_MEM)) == 0) {
         status = handle_create_shared_memory_request(fd_read,fd_write);
-    }
+    }else if(strncmp(request_name, MSG_WRITE_TO_SH_MEM, strlen(MSG_WRITE_TO_SH_MEM)) == 0) {
+        status = handle_write_to_shared_memory_request(fd_read,fd_write);
+    }else if(strncmp(request_name, MSG_MAP_FILE, strlen(MSG_MAP_FILE)) == 0) {
+        exit_loop = true;
+    }else if(strncmp(request_name, MSG_READ_FROM_FILE_OFFSET, strlen(MSG_READ_FROM_FILE_OFFSET)) == 0) {
+        exit_loop = true;
+    }else if(strncmp(request_name, MSG_READ_FROM_FILE_SECTION, strlen(MSG_READ_FROM_FILE_SECTION)) == 0) {
+        exit_loop = true;
+    }else if(strncmp(request_name, MSG_READ_FROM_LOGICAL_SPACE_OFFSET, strlen(MSG_READ_FROM_LOGICAL_SPACE_OFFSET)) == 0) {
+        exit_loop = true;
+    }else if(strncmp(request_name, MSG_EXIT, strlen(MSG_EXIT)) == 0)
+        exit_loop = true;
     clean_up:
     return status;
 }
-int handle_ping_request(int fd_write){
+int handle_ping_request(){
     int status = SUCCESS;
     char MSG_PONG[] = "PONG";
     int ID_PING = 41938;
@@ -171,25 +190,24 @@ int handle_ping_request(int fd_write){
         printf("%s\n%s",MSG_ERROR, select_error_message(status));
     return status;
 }
-int handle_create_shared_memory_request(int fd_read, int fd_write){
+int handle_create_shared_memory_request(){
     int status = SUCCESS;
-    int shared_mem_size = 0;
-    int shm_fd = -1;
-    char shared_mem_name[] = "/o9gGHlSV";
-    char * shared_mem_data = NULL;
 
+    unsigned int shared_mem_size;
     status = read_number_field(fd_read, &shared_mem_size);
     if(status != SUCCESS) goto clean_up;
+    if(shared_mem_size != SH_MEM_SIZE)
+        return ERR_CREATING_SHARED_MEMORY;
 
-    status = write_string_field(fd_write, MSG_SH_MEM);
+    status = write_string_field(fd_write, MSG_CREATE_SH_MEM);
     if(status != SUCCESS) goto clean_up;
 
-    status = create_shared_memory(&shm_fd, shared_mem_name, shared_mem_size);
+    status = create_shared_memory(&fd_shm, SH_MEM_NAME, SH_MEM_SIZE);
     if(status == SUCCESS) {
         status = write_string_field(fd_write, MSG_SUCCESS);
         if(status != SUCCESS) goto clean_up;
         /* if the shared memory was created successfully, map it to the program's VAS */
-        status = map_shared_memory(shm_fd,shared_mem_size,shared_mem_data);
+        status = map_shared_memory(fd_shm, SH_MEM_SIZE, shared_mem_data);
     }else {
         status = write_string_field(fd_write, MSG_ERROR);
     }
@@ -197,13 +215,49 @@ int handle_create_shared_memory_request(int fd_read, int fd_write){
     clean_up:
     if(status != SUCCESS)
         printf("%s\n%s",MSG_ERROR, select_error_message(status));
-    if(shm_fd > 0) {
-        munmap(shared_mem_data,shared_mem_size);
-        close(shm_fd);
-        shm_unlink(shared_mem_name);
-    }
     return status;
 }
+
+int handle_write_to_shared_memory_request(){
+    int status = SUCCESS;
+    unsigned int offset;
+    unsigned int value;
+
+    status = read_number_field(fd_read, &offset);
+    if(status != SUCCESS) goto clean_up;
+
+    status = read_number_field(fd_read, &value);
+    if(status != SUCCESS) goto clean_up;
+
+    status = write_string_field(fd_write, MSG_WRITE_TO_SH_MEM);
+    if(status != SUCCESS) goto clean_up;
+
+    /* validate the offset */
+    if(offset < 0 || offset > SH_MEM_SIZE)
+        status = ERR_WRITING_TO_SHARED_MEMORY;
+    /* validate if the bytes of the written value also correspond to offsets inside shared memory */
+    unsigned int limit = offset + sizeof(value);
+    if(limit > SH_MEM_SIZE)
+        status = ERR_WRITING_TO_SHARED_MEMORY;
+
+    if(status == SUCCESS) {
+        status = lseek(fd_shm,offset,SEEK_SET);
+        if(status != offset) goto clean_up;
+
+        status = write(fd_shm,&value,sizeof(unsigned int));
+        if(status != sizeof(unsigned int)) goto clean_up;
+
+        status = write_string_field(fd_write, MSG_SUCCESS);
+    }else {
+        status = write_string_field(fd_write, MSG_ERROR);
+    }
+
+    clean_up:
+    if(status != SUCCESS)
+        printf("%s\n%s",MSG_ERROR, select_error_message(status));
+    return status;
+}
+
 int create_shared_memory(int * fd, char * name, int size){
     /* create shared memory region */
     *fd = shm_open(name, O_CREAT | O_RDWR, 0664);
@@ -221,28 +275,8 @@ int map_shared_memory(int fd, int size, char * mapped_data) {
         return ERR_CREATING_MAPPING;
     return SUCCESS;
 }
-bool is_valid_sf_format(sf_header_t sf_header){
-    /* check magic number = MAGIC_NR */
-    char magic_ext[5];
-    memcpy(magic_ext,sf_header.magic,strlen(sf_header.magic));
-    magic_ext[4] = '\0';
-    if(strcmp(magic_ext,MAGIC_NR) != 0)
-        return false;
-    /* check version number in range:  14 - 128 */
-    if(sf_header.version < 47 || sf_header.version > 128)
-        return false;
-    /* check no_of_sections in range : 3 - 19 */
-    if((int)sf_header.no_of_sections < 5 || (int)sf_header.no_of_sections > 19)
-        return false;
-    /* check valid sect type in {19, 10 , 58, 57, 11, 53} */
-    bool valid = false;
-    const unsigned int VALID_SECT_TYPES[] = {19, 10 , 58, 57, 11, 53};
-    int n = sizeof(VALID_SECT_TYPES) / sizeof(int);
-    for(int i=0;i<n;i++)
-        if(sf_header.no_of_sections == VALID_SECT_TYPES[i])
-            valid = true;
-    return valid;
-}
+
+
 int create_named_pipe(char * name) {
     if (mkfifo(name, 0600) == -1) {
         return ERR_CREATING_PIPE;
@@ -280,9 +314,47 @@ int write_number_field(int fd, unsigned int param){
         return ERR_WRITING_TO_PIPE;
     return SUCCESS;
 }
-int read_number_field(int fd, int * param){
+int read_number_field(int fd, unsigned int * param){
     size_t size = sizeof(unsigned int);
     if(read(fd, param, size) < size)
         return ERR_READING_FROM_PIPE;
     return SUCCESS;
 }
+
+char * select_error_message(return_status_t status){
+    switch (status) {
+        case ERR_CREATING_PIPE: return "cannot create pipe";
+        case ERR_OPENING_PIPE: return "cannot open pipe";
+        case ERR_READING_FROM_PIPE: return "cannot read from request pipe";
+        case ERR_WRITING_TO_PIPE: return "cannot write to response pipe";
+        case ERR_CREATING_SHARED_MEMORY: return "cannot create shared memory";
+        case ERR_CREATING_MAPPING: return "cannot map shared memory";
+        case ERR_WRITING_TO_SHARED_MEMORY: return "cannot write to shared memory";
+        default: return "";
+    }
+}
+
+
+bool is_valid_sf_format(sf_header_t sf_header){
+    /* check magic number = MAGIC_NR */
+    char magic_ext[5];
+    memcpy(magic_ext,sf_header.magic,strlen(sf_header.magic));
+    magic_ext[4] = '\0';
+    if(strcmp(magic_ext,MAGIC_NR) != 0)
+        return false;
+    /* check version number in range:  14 - 128 */
+    if(sf_header.version < 47 || sf_header.version > 128)
+        return false;
+    /* check no_of_sections in range : 3 - 19 */
+    if((int)sf_header.no_of_sections < 5 || (int)sf_header.no_of_sections > 19)
+        return false;
+    /* check valid sect type in {19, 10 , 58, 57, 11, 53} */
+    bool valid = false;
+    const unsigned int VALID_SECT_TYPES[] = {19, 10 , 58, 57, 11, 53};
+    int n = sizeof(VALID_SECT_TYPES) / sizeof(int);
+    for(int i=0;i<n;i++)
+        if(sf_header.no_of_sections == VALID_SECT_TYPES[i])
+            valid = true;
+    return valid;
+}
+
